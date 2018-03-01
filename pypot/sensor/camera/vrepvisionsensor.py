c4ce6d97c2448c70a5dc31779a32a773007dd807
Jose Ruiz <jruizj6@gmail.com>
2018-03-01 13:13:31 +0100

    code refactor

--- pypot/sensor/camera/vrepvisionsensor.py
+++ pypot/sensor/camera/vrepvisionsensor.py
@@ -6,25 +6,28 @@ class VrepVisionSensor(AbstractCamera):
     registers = AbstractCamera.registers
     simulator_only = True
 
-    def __init__(self, name, vrep_io):
+    def __init__(self, name, fps, vrep_io):
         try:
             vision_sensor_handler = vrep_io.get_object_handle(name)
         except:
             raise Exception('Can not load ' + name + ' from V-REP')
-        resolution, image = vrep_io.get_vision_sensor_image(vision_sensor_handler, streaming=True)
+        self._res, image = vrep_io.get_vision_sensor_image(vision_sensor_handler, streaming=True)
 
         def get_vision_sensor_image():
             return vrep_io.get_vision_sensor_image(vision_sensor_handler, buffer=True)
         self._grab = get_vision_sensor_image
-        AbstractCamera.__init__(self, name, resolution, 30)
+        AbstractCamera.__init__(self, name, self._res, fps)
 
-    def grab(self):
-        resolution, image = self._grab()
+    def post_processing(self, image):
+        print self.name
+        if image is None:
+            return None
         image = np.array(image, dtype=np.uint8)
-        image.resize([resolution[0], resolution[1], 3])
+        image.resize([self._res[0], self._res[1], 3])
         image = np.rot90(image, 2)
         image = np.fliplr(image)
         return image
 
-    def close(self):
-        AbstractCamera.close()
+    def grab(self):
+        self._res, image = self._grab()
+        return image
