167b35af2611a11da932cfcb11c7ce339d979ba8
Jose Ruiz <jruizj6@hotmail.com>
2018-03-19 13:39:59 +0100

    adding color filter to image grabbed from camera

--- pypot/sensor/camera/opencvcam.py
+++ pypot/sensor/camera/opencvcam.py
@@ -9,12 +9,12 @@ class OpenCVCamera(AbstractCamera):
     def __init__(self, name, index, fps, resolution=None):
         self._index = index
         self.capture = cv2.VideoCapture(self.index)
-        if not self.capture.isOpened():
-            raise ValueError('Can not open camera device {}. You should start your robot with argument camera=\'dummy\'. E.g. p = PoppyErgoJr(camera=\'dummy\')'.format(index))
-
+        self.capture.set(cv2.CAP_PROP_FPS, fps)
         if resolution is not None:
             self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
             self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
+        if not self.capture.isOpened():
+            raise ValueError('Can not open camera device {}. You should start your robot with argument camera=\'dummy\'. E.g. p = PoppyErgoJr(camera=\'dummy\')'.format(index))
 
         AbstractCamera.__init__(self, name, resolution, fps)
 
@@ -24,9 +24,9 @@ class OpenCVCamera(AbstractCamera):
 
     def grab(self):
         rval, frame = self.capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         if not rval:
             raise EnvironmentError('Can not grab image from the camera!')
-
         return frame
 
     def close(self):
