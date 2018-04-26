d1ae9a8464db7bed4283d1bec352ee8de5c4465a
Jose Ruiz <jruizj6@hotmail.com>
2018-04-26 17:18:33 +0200

    support for multiple robots via http requests

--- pypot/creatures/imitator.py
+++ pypot/creatures/imitator.py
@@ -1,19 +1,21 @@
+import requests
+import json
 
 class Imitator(object):
-    def __init__(self, real_robot=None, simulated_robot=None):
+    def __init__(self, remote_robots=[], simulated_robot=None):
         self._simulated_robot = simulated_robot
-        self._real_robot = real_robot
+        self._remote_robots = remote_robots
 
     @property
     def simulated_robot(self):
-        return self.simulated_robot
+        return self._simulated_robot
 
     @property
-    def real_robot(self):
-        return self._real_robot
+    def remote_robots(self):
+        return self._remote_robots
 
-    def _run_command(self, robot_name, command, get_value=True):
-        formatted_command = '{}.{}'.format(robot_name, command)
+    def _run_command_on_simulation(self, command, get_value=True):
+        formatted_command = 'self.simulated_robot.{}'.format(command)
         if get_value:
             return eval(formatted_command)
         else:
@@ -21,10 +23,16 @@ class Imitator(object):
             return None
 
     def imitate(self, command, get_value=True):
-        real_result = None
-        simulated_result = None
-        if self.real_robot:
-            real_result = self._run_command('self.real_robot', command, get_value)
+        results = []
         if self.simulated_robot:
-            simulated_result = self._run_command('self.simulated_robot', command, get_value)
-        return real_result, simulated_result
+            simulated_result = self._run_command_on_simulation(command, get_value)
+            results.append({'result': simulated_result})
+        for robot_ip_and_port in self._remote_robots:
+            request_body = {'instruction': command, 'get': get_value}
+            request = requests.post('http://{}/run_instruction'.format(robot_ip_and_port),
+                                    data=json.dumps(request_body),
+                                    headers={'content-type': 'application/json'})
+            response = request.json()
+            results.append(response)
+
+        return results
