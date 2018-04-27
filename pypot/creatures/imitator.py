7863c6be6836d7a5cedf37a6f17c214c9d157a32
Jose Ruiz <jruizj6@hotmail.com>
2018-04-27 13:25:06 +0200

    async requests

--- pypot/creatures/imitator.py
+++ pypot/creatures/imitator.py
@@ -1,10 +1,13 @@
 import requests
 import json
+from threading import Thread
+
 
 class Imitator(object):
     def __init__(self, remote_robots=[], simulated_robot=None):
         self._simulated_robot = simulated_robot
         self._remote_robots = remote_robots
+        self._responses = []
 
     @property
     def simulated_robot(self):
@@ -22,17 +25,26 @@ class Imitator(object):
             exec formatted_command
             return None
 
+    def _get_response(self, ip_and_port, payload):
+        try:
+            request = requests.post('http://{}/run_instruction'.format(ip_and_port),
+                                    data=json.dumps(payload),
+                                    headers={'content-type': 'application/json'},
+                                    timeout=5)
+            response = request.json()
+            self._responses.append(response)
+        except requests.exceptions.ConnectionError as error:
+            self._responses.append({'error': error})
+
     def imitate(self, command, get_value=True):
-        results = []
+        self._responses = []
+        expected_responses = len(self._remote_robots)
         if self.simulated_robot:
             simulated_result = self._run_command_on_simulation(command, get_value)
-            results.append({'result': simulated_result})
+            self._responses.append({'result': simulated_result})
         for robot_ip_and_port in self._remote_robots:
             request_body = {'instruction': command, 'get': get_value}
-            request = requests.post('http://{}/run_instruction'.format(robot_ip_and_port),
-                                    data=json.dumps(request_body),
-                                    headers={'content-type': 'application/json'})
-            response = request.json()
-            results.append(response)
-
-        return results
+            Thread(name=robot_ip_and_port, target=self._get_response, args=(robot_ip_and_port, request_body,)).start()
+        while len(self._responses) != expected_responses:
+            pass
+        return self._responses
