88341fac52c81f67fe868888976cf5bea1aa1c74
Jose Ruiz <jruizj6@hotmail.com>
2018-05-11 18:35:23 +0200

    Bilateral communication

--- pypot/sensor/arduino/ArduinoProtocol/ArduinoProtocol.ino
+++ pypot/sensor/arduino/ArduinoProtocol/ArduinoProtocol.ino
@@ -1,12 +1,12 @@
+// Header start codes
 const uint8_t OUTPUT_HEADER_START_VALUE = 0x7e;
 const uint8_t INPUT_HEADER_START_VALUE = 0x7c;
-
-const int ledBuiltIn =  LED_BUILTIN;// the number of the LED pin
-
+// the number of the LED pin
+const int ledBuiltIn =  LED_BUILTIN;
+// Frequency interval varaibles
 unsigned long previousTransmitMillis = 0;
 unsigned long transmitMillis = millis();
 unsigned long transmitInterval = 5000;
-
 /** Messages Codes **/
 const int STATUS_MESSAGE_CODE = 0x00;
 const int MOVE_MOTORS_MESSAGE_CODE = 0x01;
@@ -14,7 +14,6 @@ const int PLAY_SOUND_MESSAGE_CODE = 0x02;
 const int CONFIG_FREQUENCY_MESSAGE_CODE = 0x03;
 const int ODROID_ACK_MESSAGE_CODE = 0x10;
 const int ARDUINO_ACK_MESSAGE_CODE = 0x11;
-
 /** Header Buffer Registers **/
 // Output Header
 const uint8_t OUTPUT_HEADER_BUFFER_SIZE = 0x04;
@@ -24,7 +23,6 @@ const int HEADER_START_CODE = 0;
 const int HEADER_MESSAGE_TYPE = 1;
 const int HEADER_DATA_SIZE = 2;
 const int HEADER_CHECKSUM = 3;
-
 /** Data Buffer Registers **/
 // Output Buffer
 const uint8_t OUTPUT_DATA_BUFFER_SIZE = 0x11;
@@ -52,17 +50,27 @@ const int DATA_MOTOR_2_0 = 14;
 const int DATA_MOTOR_2_1 = 15;
 // Checksum position
 const int DATA_CHECKSUM = 16;
-
 // Receiver Variables
+const int TRACK_LENGTH_DATA_SIZE = 2;
+const int FREQUENCY_DATA_SIZE = 4;
+const int MOTOR_POSITION_DATA_SIZE = 4;
 const uint8_t INPUT_HEADER_BUFFER_SIZE = 0x04;
 uint8_t inputHeaderBuffer[INPUT_HEADER_BUFFER_SIZE];
 int inputHeaderActualPosition = 0;
 bool inputHeaderFound = false;
 bool isHeader = false;
-uint8_t soundLengthBytes[2];
-uint8_t soundTrack = 0x00;
+uint8_t trackLengthBytes[TRACK_LENGTH_DATA_SIZE];
+uint8_t motorPositionBytes[MOTOR_POSITION_DATA_SIZE];
+uint8_t trackNumber = 0x00;
 uint8_t trackLength = 0x0000;
+uint8_t motor1Direction = 0x00;
+uint8_t motor2Direction = 0x00;
+uint8_t motor1Position = 0x00000000;
+uint8_t motor2Position = 0x00000000;
 
+bool messageReceived = false;
+
+/* Generates a checksum from the given buffer */
 uint8_t buffer_checksum(uint8_t pBuffer[], uint8_t bufferSize) {
   uint8_t result = 0;
   uint16_t sum = 0;
@@ -73,25 +81,47 @@ uint8_t buffer_checksum(uint8_t pBuffer[], uint8_t bufferSize) {
   return result;
 }
 
-/************************************************************************/
-/************************************************************************/
-/************************************************************************/
-/*
- * Receiver Methods
- */
+/************************************************************************
+ ************************Receiver Methods********************************
+ ************************************************************************/
+
+/* Verify if the header message type is valid */
+bool isValidHeaderMessageType(uint8_t pMessageType){
+  switch (pMessageType){
+    case MOVE_MOTORS_MESSAGE_CODE:
+      break;
+    case PLAY_SOUND_MESSAGE_CODE:
+      break;
+    case CONFIG_FREQUENCY_MESSAGE_CODE:
+      break;
+    case ODROID_ACK_MESSAGE_CODE:
+      break;
+    default:
+      return false;
+  }
+  return true;
+}
+
+/* Returns a value from separated bytes 
+ * For example: from bytes 0x07, 0x81 returns 0x0781 
+*/
 uint16_t parseBytesToNumber(uint8_t bytes[], int arraySize){
   uint16_t value = 0;
   for (int i = 0; i < arraySize; i++){
-    value = value * 256;
+    value = value << 8;
     value += bytes[i];
   }
   return value;
 }
 
+/* Moves the header buffer pointer to the beginning */
 void clearInputBuffers(){
   inputHeaderActualPosition = 0;
 }
 
+/* If a header message start code is found,
+ * initializes the parameters to start to read the header message
+ */
 void checkHeaderStart(uint8_t byte){
   if(byte == INPUT_HEADER_START_VALUE){
     if(!inputHeaderFound){
@@ -102,47 +132,70 @@ void checkHeaderStart(uint8_t byte){
   }
 }
 
+/* Checks if header buffer is full */
 bool isHeaderBufferFull(){
   return inputHeaderActualPosition == INPUT_HEADER_BUFFER_SIZE;
 }
 
+/* Compares the received checksum and the checksum generated with received data  */
 bool isValidChecksum(uint8_t pBuffer[], uint8_t pBufferSize, uint8_t pChecksum){
   uint8_t calculatedChecksum = buffer_checksum(pBuffer,pBufferSize);
   return calculatedChecksum == pChecksum;
 }
 
+/* Clear reciever buffers */
 void resetInputData(){
   clearInputBuffers();
   inputHeaderFound = false;
   isHeader = false;
 }
 
+/* Returns the real value of separated bytes that indicates the new motor position */
+uint8_t getMotorPositionValueFromBytes(uint8_t byte0, uint8_t byte1, uint8_t byte2, uint8_t byte3){
+  motorPositionBytes[0] = byte0;
+  motorPositionBytes[1] = byte1;
+  motorPositionBytes[2] = byte2;
+  motorPositionBytes[3] = byte3;
+  return parseBytesToNumber(motorPositionBytes, MOTOR_POSITION_DATA_SIZE);
+}
+
+/* Checks the message type and perform an specific task for each type */
 void processData(uint8_t pMessageType, uint8_t pMessageData[]){
   switch (pMessageType){
     case MOVE_MOTORS_MESSAGE_CODE:
+      motor1Direction = pMessageData[0];
+      motor2Direction = pMessageData[1];
+      motor1Position = getMotorPositionValueFromBytes(pMessageData[2], pMessageData[3], pMessageData[4], pMessageData[5]);
+      motor2Position = getMotorPositionValueFromBytes(pMessageData[6], pMessageData[7], pMessageData[8], pMessageData[9]);
+      // move motors function
       digitalWrite(ledBuiltIn, HIGH);
+      messageReceived = true;
       break;
     case PLAY_SOUND_MESSAGE_CODE:
-      soundLengthBytes[0] = pMessageData[1];
-      soundLengthBytes[1] = pMessageData[2];
-      soundTrack = pMessageData[0];
-      trackLength = parseBytesToNumber(soundLengthBytes, 2);
-      // play
+      trackLengthBytes[0] = pMessageData[1];
+      trackLengthBytes[1] = pMessageData[2];
+      trackNumber = pMessageData[0];
+      trackLength = parseBytesToNumber(trackLengthBytes, TRACK_LENGTH_DATA_SIZE);
+      // play function
       digitalWrite(ledBuiltIn, HIGH);
+      messageReceived = true;
       break;
     case CONFIG_FREQUENCY_MESSAGE_CODE:
-      transmitInterval = parseBytesToNumber(pMessageData, 4);
+      transmitInterval = parseBytesToNumber(pMessageData, FREQUENCY_DATA_SIZE);
       digitalWrite(ledBuiltIn, HIGH);
+      messageReceived = true;
       break;
     case ODROID_ACK_MESSAGE_CODE:
       digitalWrite(ledBuiltIn, HIGH);
+      messageReceived = true;
       break;
     default:
       break;
   }
 }
 
-bool processInputData(uint8_t dataBufferSize){
+/* Given the data size, reads an specific number of bytes to check if is valid */
+bool verifyIncomingMessageData(uint8_t dataBufferSize){
   if(dataBufferSize == 0){
     processData(inputHeaderBuffer[HEADER_MESSAGE_TYPE], new uint8_t[0]);
     return true;
@@ -165,67 +218,79 @@ bool processInputData(uint8_t dataBufferSize){
   }
 }
 
-void verifyIncomingData(){
+/* Read the input message header, trying to find a valid header.
+ * If a valid header is found, proceed to process the message data.
+ */
+void verifyIncomingMessageHeader(){
   int incomingByte = Serial.read();
   if (incomingByte != -1) {
     checkHeaderStart(incomingByte);
     inputHeaderBuffer[inputHeaderActualPosition] = incomingByte;
     inputHeaderActualPosition++;
     if(isHeaderBufferFull()){
-      if(isValidChecksum(inputHeaderBuffer, INPUT_HEADER_BUFFER_SIZE, incomingByte)){
-        processInputData(inputHeaderBuffer[HEADER_DATA_SIZE]);
+      if(isValidChecksum(inputHeaderBuffer, INPUT_HEADER_BUFFER_SIZE, incomingByte)
+          && isValidHeaderMessageType(inputHeaderBuffer[HEADER_MESSAGE_TYPE])){
+        verifyIncomingMessageData(inputHeaderBuffer[HEADER_DATA_SIZE]);
       }
       resetInputData();
     }
   }
 }
 
-/************************************************************************/
-/************************************************************************/
-/************************************************************************/
-/*
- * Sender Methods
- */
+/************************************************************************
+ **************************Sender Methods********************************
+ ************************************************************************/
+
+/* Fills the status message data buffer with sensors values */
 void fillStatusDataBuffer(){
   // TO_DO replace with real data 
+  // Capacitives Data
   outputDataBuffer[DATA_CAPACITIVE_1] = 0x01;
   outputDataBuffer[DATA_CAPACIVITE_2] = 0x02;
   outputDataBuffer[DATA_CAPACITIVE_3] = 0x03;
   outputDataBuffer[DATA_CAPACITIVE_4] = 0x04;
-  
+  // Microphone 1 data
   outputDataBuffer[DATA_MIC_1_0] = 0x05;
   outputDataBuffer[DATA_MIC_1_1] = 0x06;
   outputDataBuffer[DATA_MIC_1_2] = 0x07;
   outputDataBuffer[DATA_MIC_1_3] = 0x08;
-  
+  // Microphone 2 data
   outputDataBuffer[DATA_MIC_2_0] = 0x09;
   outputDataBuffer[DATA_MIC_3_1] = 0x0A;
   outputDataBuffer[DATA_MIC_4_2] = 0x0B;
   outputDataBuffer[DATA_MIC_5_3] = 0x0C;
-  
+  // Motor 1 Position
   outputDataBuffer[DATA_MOTOR_1_0] = 0x0D;
   outputDataBuffer[DATA_MOTOR_1_1] = 0x0E;
-  
+  //Motor 2 position
   outputDataBuffer[DATA_MOTOR_2_0] = 0x0F;
   outputDataBuffer[DATA_MOTOR_2_1] = 0x10;
-  // don't change this line since calculates the checksum of the data
+  // don't change this line bellow since calculates the checksum of the data
   outputDataBuffer[DATA_CHECKSUM] = buffer_checksum(outputDataBuffer, OUTPUT_DATA_BUFFER_SIZE);
 }
 
-void sendStatusMessage(){
-  outputHeaderBuffer[HEADER_MESSAGE_TYPE] = STATUS_MESSAGE_CODE;
-  outputHeaderBuffer[HEADER_DATA_SIZE] = OUTPUT_DATA_BUFFER_SIZE;
+void fillHeaderBuffer(uint8_t pMessageCode, uint8_t pDataSize){
+  outputHeaderBuffer[HEADER_MESSAGE_TYPE] = pMessageCode;
+  outputHeaderBuffer[HEADER_DATA_SIZE] = pDataSize;
   outputHeaderBuffer[HEADER_CHECKSUM] = buffer_checksum(outputHeaderBuffer, OUTPUT_HEADER_BUFFER_SIZE);
+}
+
+/* Initialize the output header to send the status message*/
+void sendStatusMessage(){
+  fillHeaderBuffer(STATUS_MESSAGE_CODE, OUTPUT_DATA_BUFFER_SIZE);
   fillStatusDataBuffer();
   Serial.write(outputHeaderBuffer, OUTPUT_HEADER_BUFFER_SIZE);
   Serial.write(outputDataBuffer, OUTPUT_DATA_BUFFER_SIZE);
 }
-/************************************************************************/
-/************************************************************************/
-/************************************************************************/
-/*
- * Arduino Functions
- */
+
+void sendAckMessage(){
+  fillHeaderBuffer(ARDUINO_ACK_MESSAGE_CODE, 0);
+  Serial.write(outputHeaderBuffer, OUTPUT_HEADER_BUFFER_SIZE);
+}
+
+/************************************************************************
+ ************************Arduino Functions*******************************
+ ************************************************************************/
 void setup() {
   pinMode(ledBuiltIn, OUTPUT);
   while (!Serial);
@@ -234,12 +299,15 @@ void setup() {
 }
 
 void loop() {
-  verifyIncomingData();
+  verifyIncomingMessageHeader();
   transmitMillis = millis();
+  if (messageReceived){
+    messageReceived = false;
+    sendAckMessage();
+  }
   if (transmitMillis - previousTransmitMillis >= transmitInterval) {
     previousTransmitMillis = transmitMillis;
     sendStatusMessage();
     digitalWrite(ledBuiltIn, LOW);
   }
 }
-
--- pypot/sensor/arduino/arduino_sensor.py
+++ pypot/sensor/arduino/arduino_sensor.py
@@ -3,21 +3,24 @@ from time import sleep
 from threading import Thread
 from ...robot.sensor import Sensor
 
-STATUS_MESSAGE_CODE = 0x00
+# OUTPUT MESSAGES
 MOVE_MOTORS_MESSAGE_CODE = 0x01
 PLAY_SOUND_MESSAGE_CODE = 0x02
 CONFIG_FREQUENCY_MESSAGE_CODE = 0x03
 ODROID_ACK_MESSAGE_CODE = 0x10
+# INPUT MESSAGES
+STATUS_MESSAGE_CODE = 0x00
 ARDUINO_ACK_MESSAGE_CODE = 0x11
-
+INPUT_MESSAGES_CODES = [STATUS_MESSAGE_CODE, ARDUINO_ACK_MESSAGE_CODE]
+# MESSAGE SIZES
 MOVE_MOTORS_MESSAGE_SIZE = 0x0b
 PLAY_SOUND_MESSAGE_SIZE = 0x04
 CONFIG_FREQUENCY_MESSAGE_SIZE = 0x05
 ODROID_ACK_MESSAGE_SIZE = 0x00
-
-HEADER_START_SYMBOL = 0x7e
+# HEADER START VALUES
+HEADER_START_VALUE = 0x7e
 OUTPUT_HEADER_START_VALUE = 0x7c
-
+# HEADER VALUES POSITIONS
 HEADER_START_CODE = 0
 HEADER_MESSAGE_TYPE = 1
 HEADER_DATA_SIZE = 2
@@ -37,6 +40,13 @@ def generate_checksum(buffer_list):
 
 def valid_checksum(buffer_list):
     return generate_checksum(buffer_list) == buffer_list[-1]  # checksum value & 0xFF
+
+
+def is_valid_header_message_type(message_type):
+    if message_type in INPUT_MESSAGES_CODES:
+        return True
+    else:
+        return False
 
 
 class Sender(object):
@@ -69,11 +79,21 @@ class Sender(object):
     def _generate_output_data_checksum(self):
         self._output_data_buffer[-1] = generate_checksum(self._output_data_buffer)
 
-    def move_motors(self):
+    def move_motors(self, motor1_dir, motor2_dir, motor1_position, motor2_position):
         self._fill_header(MOVE_MOTORS_MESSAGE_CODE, MOVE_MOTORS_MESSAGE_SIZE)
-        self._initialize_output_data(CONFIG_FREQUENCY_MESSAGE_SIZE)
+        self._initialize_output_data(MOVE_MOTORS_MESSAGE_SIZE)
+        self._output_data_buffer[0] = motor1_dir
+        self._output_data_buffer[1] = motor2_dir
+        self._output_data_buffer[2] = split_value(motor1_position, -8, -6)
+        self._output_data_buffer[3] = split_value(motor1_position, -6, -4)
+        self._output_data_buffer[4] = split_value(motor1_position, -4, -2)
+        self._output_data_buffer[5] = split_value(motor1_position, -2, None)
+        self._output_data_buffer[6] = split_value(motor2_position, -8, -6)
+        self._output_data_buffer[7] = split_value(motor2_position, -6, -4)
+        self._output_data_buffer[8] = split_value(motor2_position, -4, -2)
+        self._output_data_buffer[9] = split_value(motor2_position, -2, None)
         self._generate_output_data_checksum()
-        self._send_data()  # TODO se debe enviar posicion nueva a ambos motores?
+        self._send_data()
 
     def play_sound(self, track, length):
         self._fill_header(PLAY_SOUND_MESSAGE_CODE, PLAY_SOUND_MESSAGE_SIZE)
@@ -107,7 +127,7 @@ class Receiver(object):
         self._input_header_size = 4
         self._input_header_found = False
         self._is_header = False
-        self._header_start_code = HEADER_START_SYMBOL
+        self._header_start_code = HEADER_START_VALUE
         self.running = True
 
     def _clear_input_buffers(self):
@@ -127,20 +147,21 @@ class Receiver(object):
 
     def _process_incoming_message(self):
         if self._input_header_buffer[HEADER_MESSAGE_TYPE] == STATUS_MESSAGE_CODE:
-            print 'status message'
+            print ('status message')
         elif self._input_header_buffer[HEADER_MESSAGE_TYPE] == ARDUINO_ACK_MESSAGE_CODE:
-            print 'Ack message'
+            print ('Arduino ack message')
 
-    def _read_data(self, buffer_size):
+    def _is_valid_data_message(self, buffer_size):
         if buffer_size == 0:  # message without data
-            print 'data 0'
+            # print 'data 0'
+            self._process_incoming_message()
             return True
         while buffer_size > 0:
             hex_input = self._get_byte()
             if hex_input is not None:
                 self._input_data_buffer.append(hex_input)
                 buffer_size -= 1
-        print self._input_data_buffer
+        # print self._input_data_buffer
         if valid_checksum(self._input_data_buffer):
             self._process_incoming_message()
             return True
@@ -166,8 +187,10 @@ class Receiver(object):
                 self._input_header_buffer.append(hex_input)
                 if self._header_buffer_full():
                     print self._input_header_buffer
-                    if valid_checksum(self._input_header_buffer):
-                        self._read_data(self._input_header_buffer[HEADER_DATA_SIZE])
+                    if (valid_checksum(self._input_header_buffer)
+                            and
+                            is_valid_header_message_type(self._input_header_buffer[HEADER_MESSAGE_TYPE])):
+                        self._is_valid_data_message(self._input_header_buffer[HEADER_DATA_SIZE])
                     self._reset_data()
 
 
@@ -195,8 +218,8 @@ class ArduinoSensor(Sensor):
         self._status_loop.start()
         self.running = True
 
-    def send_move_motors_message(self):
-        self._sender.move_motors()
+    def send_move_motors_message(self, motor1_dir, motor2_dir, motor1_position, motor2_position):
+        self._sender.move_motors(motor1_dir, motor2_dir, motor1_position, motor2_position)
 
     def send_play_sound_message(self, track, length):
         self._sender.play_sound(track, length)
