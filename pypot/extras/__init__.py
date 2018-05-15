ce3b6b0a803787bedbbdccdd0f0865c384bcbe9b
Jose Ruiz <jruizj6@hotmail.com>
2018-05-15 19:15:20 +0200

    solving name conflicts

--- pypot/extras/__init__.py
+++ pypot/extras/__init__.py
@@ -3,6 +3,6 @@ from warnings import warn
 from .tracker import Tracker
 from .image_display import ImageDisplay
 try:
-    from .face_recognition import FaceRecognition
+    from .recognition import FaceRecognition
 except ImportError:
     warn("FaceRecognition can't be imported. Missing Dependencies.", Warning)
